fb5d5d657745def1929d913ec5d1acc3cc81e44d
Gitar <noreply@gitar.ai>
2024-11-26 17:53:09 +0000

    [Gitar] Updating Java files

--- src/main/java/ch/usi/si/seart/treesitter/External.java
+++ src/main/java/ch/usi/si/seart/treesitter/External.java
@@ -1,70 +1,62 @@
 package ch.usi.si.seart.treesitter;
 
+import java.lang.ref.Cleaner;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.experimental.FieldDefaults;
 
-import java.lang.ref.Cleaner;
-
-@FieldDefaults(makeFinal = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
 abstract class External implements AutoCloseable {
 
-    protected long pointer;
-    private Cleaner.Cleanable cleanable;
+  protected final long pointer;
+  private final Cleaner.Cleanable cleanable;
 
-    private static final Cleaner CLEANER = Cleaner.create();
+  private static final Cleaner CLEANER = Cleaner.create();
 
-    protected External(long pointer) {
-        this.pointer = pointer;
-        this.cleanable = CLEANER.register(this, new Action(this));
-    }
+  protected External(long pointer) {
+    this.pointer = pointer;
+    this.cleanable = CLEANER.register(this, new Action(this));
+  }
 
-    /**
-     * Checks whether the memory address associated
-     * with this external resource is {@code nullptr}.
-     *
-     * @return {@code true} if the memory address is 0,
-     * otherwise {@code false}
-     */
-    public final boolean isNull() {
-        return pointer == 0;
-    }
+  /**
+   * Checks whether the memory address associated with this external resource is {@code nullptr}.
+   *
+   * @return {@code true} if the memory address is 0, otherwise {@code false}
+   */
+  public final boolean isNull() {
+    return pointer == 0;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+    External other = (External) obj;
+    return pointer == other.pointer;
+  }
+
+  @Override
+  public int hashCode() {
+    return Long.hashCode(pointer);
+  }
+
+  /** Delete the external resource, freeing all the memory that it used. */
+  @Override
+  public void close() {
+    boolean requiresCleaning = cleanable != null && !isNull();
+    if (requiresCleaning) cleanable.clean();
+  }
+
+  protected abstract void delete();
+
+  @AllArgsConstructor(access = AccessLevel.PRIVATE)
+  private static final class Action implements Runnable {
+
+    private final External external;
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        External other = (External) obj;
-        return pointer == other.pointer;
+    public void run() {
+      external.delete();
     }
-
-    @Override
-    public int hashCode() {
-        return Long.hashCode(pointer);
-    }
-
-    /**
-     * Delete the external resource, freeing all the memory that it used.
-     */
-    @Override
-    public void close() {
-        boolean requiresCleaning = cleanable != null && !isNull();
-        if (requiresCleaning) cleanable.clean();
-    }
-
-    protected abstract void delete();
-
-    @AllArgsConstructor(access = AccessLevel.PRIVATE)
-    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-    private static final class Action implements Runnable {
-
-        External external;
-
-        @Override
-        public void run() {
-            external.delete();
-        }
-    }
+  }
 }
--- src/main/java/ch/usi/si/seart/treesitter/QueryCursor.java
+++ src/main/java/ch/usi/si/seart/treesitter/QueryCursor.java
@@ -1,120 +1,112 @@
 package ch.usi.si.seart.treesitter;
 
-import lombok.AccessLevel;
-import lombok.Generated;
-import lombok.Getter;
-import lombok.experimental.FieldDefaults;
-import lombok.experimental.NonFinal;
-import org.jetbrains.annotations.NotNull;
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import lombok.Generated;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Cursor used for executing queries, carrying the state needed to process them.
- * <p>
- * The query cursor should not be shared between threads, but can be reused for many query executions.
+ *
+ * <p>The query cursor should not be shared between threads, but can be reused for many query
+ * executions.
  *
  * @since 1.0.0
  * @author Ozren Dabić
  */
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class QueryCursor extends External implements Iterable<QueryMatch> {
 
-    Node node;
-    Query query;
+  private final Node node;
+  private final Query query;
 
-    @Getter
-    @NonFinal
-    boolean executed = false;
+  private @Getter boolean executed = false;
 
-    @SuppressWarnings("unused")
-    QueryCursor(long pointer, @NotNull Node node, @NotNull Query query) {
-        super(pointer);
-        this.node = node;
-        this.query = query;
-    }
+  @SuppressWarnings("unused")
+  QueryCursor(long pointer, @NotNull Node node, @NotNull Query query) {
+    super(pointer);
+    this.node = node;
+    this.query = query;
+  }
 
-    @Override
-    protected native void delete();
+  @Override
+  protected native void delete();
 
-    /**
-     * Start running a query against a node.
-     */
-    public native void execute();
+  /** Start running a query against a node. */
+  public native void execute();
 
-    /**
-     * Set the range of bytes positions in which the query will be executed.
-     *
-     * @param startByte The start byte of the query range
-     * @param endByte The end byte of the query range
-     * @throws ch.usi.si.seart.treesitter.exception.ByteOffsetOutOfBoundsException
-     * if either argument is outside the queried node's byte range
-     * @throws IllegalArgumentException if:
-     * <ul>
-     *     <li>{@code startByte} &lt; 0</li>
-     *     <li>{@code endByte} &lt; 0</li>
-     *     <li>{@code startByte} &gt; {@code endByte}</li>
-     * </ul>
-     * @since 1.9.0
-     */
-    public native void setRange(int startByte, int endByte);
+  /**
+   * Set the range of bytes positions in which the query will be executed.
+   *
+   * @param startByte The start byte of the query range
+   * @param endByte The end byte of the query range
+   * @throws ch.usi.si.seart.treesitter.exception.ByteOffsetOutOfBoundsException if either argument
+   *     is outside the queried node's byte range
+   * @throws IllegalArgumentException if:
+   *     <ul>
+   *       <li>{@code startByte} &lt; 0
+   *       <li>{@code endByte} &lt; 0
+   *       <li>{@code startByte} &gt; {@code endByte}
+   *     </ul>
+   *
+   * @since 1.9.0
+   */
+  public native void setRange(int startByte, int endByte);
 
-    /**
-     * Set the range of row-column coordinates in which the query will be executed.
-     *
-     * @param startPoint The start point of the query range
-     * @param endPoint The end point of the query range
-     * @throws NullPointerException if either argument is null
-     * @throws IllegalArgumentException if any point coordinates are negative,
-     * or if {@code startPoint} is a position that comes after {@code endPoint}
-     * @throws ch.usi.si.seart.treesitter.exception.PointOutOfBoundsException
-     * if any of the arguments is outside the queried node's position range
-     * @since 1.9.0
-     */
-    public native void setRange(@NotNull Point startPoint, @NotNull Point endPoint);
+  /**
+   * Set the range of row-column coordinates in which the query will be executed.
+   *
+   * @param startPoint The start point of the query range
+   * @param endPoint The end point of the query range
+   * @throws NullPointerException if either argument is null
+   * @throws IllegalArgumentException if any point coordinates are negative, or if {@code
+   *     startPoint} is a position that comes after {@code endPoint}
+   * @throws ch.usi.si.seart.treesitter.exception.PointOutOfBoundsException if any of the arguments
+   *     is outside the queried node's position range
+   * @since 1.9.0
+   */
+  public native void setRange(@NotNull Point startPoint, @NotNull Point endPoint);
 
-    /**
-     * Advance to the next match of the currently running query.
-     *
-     * @return A match if there is one, null otherwise
-     * @throws IllegalStateException if the query was not executed beforehand
-     * @see #execute()
-     */
-    public native QueryMatch nextMatch();
+  /**
+   * Advance to the next match of the currently running query.
+   *
+   * @return A match if there is one, null otherwise
+   * @throws IllegalStateException if the query was not executed beforehand
+   * @see #execute()
+   */
+  public native QueryMatch nextMatch();
 
-    /**
-     * Returns an iterator over the query matches,
-     * starting from the first {@link QueryMatch}.
-     * Implicitly calls {@link #execute()}.
-     *
-     * @return an iterator over query cursor matches
-     */
-    @Override
-    public @NotNull Iterator<QueryMatch> iterator() {
-        execute();
-        return new Iterator<>() {
+  /**
+   * Returns an iterator over the query matches, starting from the first {@link QueryMatch}.
+   * Implicitly calls {@link #execute()}.
+   *
+   * @return an iterator over query cursor matches
+   */
+  @Override
+  public @NotNull Iterator<QueryMatch> iterator() {
+    execute();
+    return new Iterator<>() {
 
-            private QueryMatch current = nextMatch();
+      private final QueryMatch current = nextMatch();
 
-            @Override
-            public boolean hasNext() {
-                return current != null;
-            }
+      @Override
+      public boolean hasNext() {
+        return current != null;
+      }
 
-            @Override
-            public QueryMatch next() {
-                if (!hasNext()) throw new NoSuchElementException();
-                QueryMatch match = current;
-                current = nextMatch();
-                return match;
-            }
-        };
-    }
+      @Override
+      public QueryMatch next() {
+        if (!hasNext()) throw new NoSuchElementException();
+        QueryMatch match = current;
+        current = nextMatch();
+        return match;
+      }
+    };
+  }
 
-    @Override
-    @Generated
-    public String toString() {
-        return String.format("QueryCursor(node: %s, query: %s)", node, query);
-    }
+  @Override
+  @Generated
+  public String toString() {
+    return String.format("QueryCursor(node: %s, query: %s)", node, query);
+  }
 }
